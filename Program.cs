c5f6ac41e5789a336d24e221e6901cec00a24322
Anton WishnevSky <h3mp_x@mail.ru>
2023-02-18 03:34:02 +0300

    shift + alt + f pressed

--- Program.cs
+++ Program.cs
@@ -14,9 +14,9 @@
 
 void ShowArray(string[] array)
 {
-for (int i = 0; i < array.Length; i++)
-Console.Write(array[i] + " ");
-Console.WriteLine();
+    for (int i = 0; i < array.Length; i++)
+        Console.Write(array[i] + " ");
+    Console.WriteLine();
 }
 
 string[] array = { "hello", "2", "world", ":-)", "1234", "1567", "-2" };

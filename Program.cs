d4d00472b0b13d2c9f31efcf898917c85298073c
Anton WishnevSky <h3mp_x@mail.ru>
2023-02-18 03:30:19 +0300

    method for finding strings of the required length (string <= 3) added

--- Program.cs
+++ Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[] ShowCutArray(string[] array)
+{
+    string[] newArray = new string[array.Length];
+    int size = 0;
+    for (int i = 0; i < array.Length; i++)
+        if (array[i].Length <= 3)
+        {
+            newArray[size] = array[i];
+            size++;
+        }
+    Array.Resize(ref newArray, size);
+    return newArray;
+}
